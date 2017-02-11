492a3f676ee72f89620200ba152fcff3113534c6
jonmanuel <jonmanuel01@gmail.com>
2017-02-10 23:26:20 -0300

    2

---
+++ main-2.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(event) 
+{
+  // Button Builder
+  var ButtonCreator = function(container) 
+  {
+    this.container = container;
+    this.buttons = [];
+  };
+
+  ButtonCreator.prototype.appendButton = function(number) 
+  {
+    // Do not change this method, is not part of the test
+    var element = document.createElement('button');
+    element.innerText = 'Button #' + (number + 1);
+    this.container.appendChild(element);
+    this.buttons.push(element);
+  };
+
+  ButtonCreator.prototype.addEvent = function(j) 
+  {
+      this.buttons[j].onclick = function(event) {
+         alert('Button #' + (j+1) + ' pressed!');
+      }
+  
+  };
+  // Initialization: Use button builder to generate 10 buttons
+  var builder = new ButtonCreator(document.getElementById('container'));
+  for (var i = 0; i < 10; ++i)
+  {
+    builder.appendButton(i);
+    builder.addEvent(i);
+  }
+  
+});
+/*
+https://github.com/jonmanuel01/Test10022017/blob/master/main.js
+
+
+*/
